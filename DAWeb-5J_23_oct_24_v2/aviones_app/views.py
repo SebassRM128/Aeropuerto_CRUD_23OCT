01423f0d85ed6943f102e6ba7491a114262ee212
SebassRM128 <a.22308051281303@cbtis128.edu.mx>
2024-10-24 12:06:25 -0600

    Add files via upload

---
+++ DAWeb-5J_23_oct_24_v2/aviones_app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Aviones
+# Create your views here.
+def listadoAviones(request):
+    losAviones=Aviones.objects.all()
+    return render(request, "gestionarAviones.html",{"misaviones":losAviones})
